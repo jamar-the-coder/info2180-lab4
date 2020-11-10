d30baf52d0bf6b27a6b5a61daa0608d0d55e8109
Jamar Lee <jamar.lee30@gmail.com>
2020-11-09 21:17:01 -0500

    Added alert functionality to app.js according to Exercise 2.

---
+++ app.js
@@ -0,0 +1,24 @@
+	"use strict"
+document.addEventListener("DOMContentLoaded", function(){
+    const buttonForSearch = document.getElementsByTagName('button')[0];
+    const httpRequestMadeByUser = new XMLHttpRequest();
+
+    buttonForSearch.addEventListener("click", function(event){
+        event.preventDefault();
+
+        httpRequestMadeByUser.onreadystatechange = function(){
+            if (httpRequestMadeByUser.readyState == 4){
+                if (httpRequestMadeByUser.status == 200){
+                    alert(httpRequestMadeByUser.responseText);
+                }
+
+                if (httpRequestMadeByUser.status == 404){
+                    alert("Error! File not found");
+                }
+            }
+        }
+
+        httpRequestMadeByUser.open('get', 'http://localhost:8080/superheroes.php', true);
+        httpRequestMadeByUser.send();
+    });
+}); 
